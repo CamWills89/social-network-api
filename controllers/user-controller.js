fccd760efd8e446f530772bf4bb83c2904bacb91
Cameron <cameron.wills89@outlook.com>
2020-11-25 09:42:13 -0500

    added basic CRUD actions and routes for Users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,93 @@
-const { User } = require('../models');
-const mongoose = require('mongoose');
+const { User } = require("../models");
+const mongoose = require("mongoose");
 
-const userController = {};
+const userController = {
+  // GET /api/users
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: "thoughts",
+        //we dont want the __v included in the thoughts
+        select: "-__v",
+      })
+      //we dont want the __v included in the pizza data either
+      .select("-__v")
+      //newest user returns first using sort (-1, says in DESC order by id value)
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // GET /api/users/id
+  // get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // POST /api/users
+  //create user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // PUT /api/users/id
+  //update User by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // DELETE /api/users/id
+  //delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //add a Friend
+  addFriend() {},
+
+  //remove a Friend
+  removeFriend() {},
+};
 
 module.exports = userController;
